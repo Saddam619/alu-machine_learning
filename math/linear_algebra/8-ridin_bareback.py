7cc3e2ac782e5f66996f0901372a5eda29b5b1e7
Saddam619 <s.adam@alustudent.com>
2026-09-20 03:29:48 -0700

    8ridin

---
+++ math/linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Module that performs matrix multiplication."""
+
+
+def mat_mul(mat1, mat2):
+    """Return the product of two 2D matrices as a new matrix."""
+    if len(mat1[0]) != len(mat2):
+        return None
+    result = []
+    for i in range(len(mat1)):
+        row = []
+        for j in range(len(mat2[0])):
+            total = 0
+            for k in range(len(mat2)):
+                total += mat1[i][k] * mat2[k][j]
+            row.append(total)
+        result.append(row)
+    return result
