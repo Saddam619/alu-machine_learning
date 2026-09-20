b429b64810db74e5bca0c71b22ff425649debfaa
Saddam619 <s.adam@alustudent.com>
2026-09-20 03:27:11 -0700

    6-howdy

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module that concatenates two arrays."""
+
+
+def cat_arrays(arr1, arr2):
+    """Return a new list holding the elements of both arrays."""
+    return arr1 + arr2
